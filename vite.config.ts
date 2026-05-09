9c1f0fb9c3b86db97dca84c7b449f0087be94795
Edgar Valli <edgarvalli80@gmail.com>
2026-05-08 18:20:44 -0600

    Se soluciona el componente para poder instalar

--- vite.config.ts
+++ vite.config.ts
@@ -11,16 +11,32 @@ export default defineConfig({
       entryRoot: "src",
     }),
   ],
+
+  optimizeDeps: {
+    exclude: ["react", "react/jsx-runtime", "react-dom"],
+  },
+
   build: {
     copyPublicDir: false,
+
+    target: "esnext",
+
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       formats: ["es"],
       fileName: () => "index.es.js",
     },
+
+    commonjsOptions: {
+      include: [],
+    },
+
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react/jsx-runtime", "react-dom"],
+
       output: {
+        preserveModules: false,
+
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
